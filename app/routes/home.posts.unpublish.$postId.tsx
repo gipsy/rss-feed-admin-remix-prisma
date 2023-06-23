0ac7eab460d5cb1ba97d942a01065e5476cc334a
gipsy <ter4444@gmail.com>
2023-06-23 15:11:56 +0300

    fix unpublish redirect

--- app/routes/home.posts.unpublish.$postId.tsx
+++ app/routes/home.posts.unpublish.$postId.tsx
@@ -1,7 +1,12 @@
 import type { ActionFunction } from "@remix-run/node"
-import { unPublishPost } from "~/utils/post.server"
+import { unPublishPost }       from "~/utils/post.server"
+import { redirect }            from "@remix-run/node";
+import { redirectBack }        from "remix-utils";
 
 export const action: ActionFunction = async ({ request, params }) => {
   const { postId } = params
-  return unPublishPost(postId)
+  
+  const url = new URL(request.url)
+  const filter = url.searchParams.get('filter')
+  return await unPublishPost(postId) && redirect(`/home/${!!filter ? 'filter='+filter : ''}`)
 }
